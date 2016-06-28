8aaccb76b79ca8840c990b5cc1979f02000d8596
HarolHiguera <harolhiguera2005@gmail.com>
2016-06-28 14:42:09 +0900

    Moving Background for the game

--- 2dGame/app/src/main/java/com/inngeniun/a2dgame/GamePanel.java
+++ 2dGame/app/src/main/java/com/inngeniun/a2dgame/GamePanel.java
@@ -1,6 +1,8 @@
 package com.inngeniun.a2dgame;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,44 +11,53 @@ import android.view.SurfaceView;
  * Created by Harol Higuera on 6/28/16.
  */
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
-
+    public static final int WIDTH = 856;
+    public static final int HEIGHT = 480;
     private MainThread thread;
+    private Background bg;
+
+    public GamePanel(Context context) {
+        super(context);
 
 
-    public GamePanel(Context ctn){
-        super(ctn);
-
+        //add the callback to the surfaceholder to intercept events
         getHolder().addCallback(this);
 
         thread = new MainThread(getHolder(), this);
 
+        //make gamePanel focusable so it can handle events
+        setFocusable(true);
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-        thread.setRunning(true);
-        thread.start();
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+    public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
-        while (retry){
+        while (retry) {
             try {
                 thread.setRunning(false);
                 thread.join();
-            }catch (InterruptedException e){
+
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-                retry = false;
             }
+            retry = false;
         }
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
+        bg.setVector(-1);
+        //we can safely start the game loop
+        thread.setRunning(true);
+        thread.start();
+
     }
 
     @Override
@@ -56,5 +67,20 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     public void update() {
 
+        bg.update();
     }
+
+    @Override
+    public void draw(Canvas canvas) {
+        final float scaleFactorX = getWidth() / WIDTH;
+        final float scaleFactorY = getHeight() / HEIGHT;
+        if (canvas != null) {
+            final int savedState = canvas.save();
+            canvas.scale(scaleFactorX, scaleFactorY);
+            bg.draw(canvas);
+            canvas.restoreToCount(savedState);
+        }
+    }
+
 }
+
